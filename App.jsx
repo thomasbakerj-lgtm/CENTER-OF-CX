acf24ff51810daad23ae0faca12d75fc762b6cf2
thomasbakerj-lgtm <thomasbakerj@gmail.com>
2026-04-05 22:55:30 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './Homepage'
 import PlatformsTech from './PlatformsTech'
+import About from './About'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/platforms-and-tech" element={<PlatformsTech />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
